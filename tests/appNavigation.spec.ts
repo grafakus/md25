e43671a2c889daecf2314d9c7b2870f6fbe6a276
Marc Mignonsin <marc.mignonsin@grafana.com>
2025-02-27 18:49:24 +0100

    test(E2E): Remove irrelevant test

--- tests/appNavigation.spec.ts
+++ tests/appNavigation.spec.ts
@@ -2,11 +2,6 @@ import { test, expect } from './fixtures';
 import { ROUTES } from '../src/constants';
 
 test.describe('navigating app', () => {
-  test('page Hello World should render successfully', async ({ gotoPage, page }) => {
-    await gotoPage(`/${ROUTES.HelloWorld}`);
-    await expect(page.getByText('Hello world panel')).toBeVisible();
-  });
-
   test('page With Tabs should render successfully', async ({ gotoPage, page }) => {
     await gotoPage(`/${ROUTES.WithTabs}`);
     await expect(page.getByText('This scene showcases a basic tabs functionality.')).toBeVisible();
